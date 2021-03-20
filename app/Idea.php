a5ed77d290267450869428a0474df8a3075b95ef
He Xiaocong <hxc93@connect.hku.hk>
2021-03-20 11:40:04 +0800

    controller

--- app/Idea.php
+++ app/Idea.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Idea extends Model
 {
-    //
+  
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'travel_id', 
+        'user_id', 
+        'title',
+        'destination',
+        'start_date',
+        'end_date'
+    ];
 }
