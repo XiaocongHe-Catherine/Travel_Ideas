4e473070abd52b2c7a12c8c1e3b0319545d9bff6
benoittravers <benoit@travers.pro>
2021-03-20 10:55:54 +0800

    Comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+
+            'comment_id',
+            'user_id',
+            'travel_id',
+            'title',
+    ];
 }
