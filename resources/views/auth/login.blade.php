5026d7cd1550264e1bdb0fd929722646858a8a27
benoittravers <benoit@travers.pro>
2021-04-19 17:22:09 +0800

    Live comments and table changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -58,6 +58,9 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
                                 @endif
                             </div>
                         </div>
